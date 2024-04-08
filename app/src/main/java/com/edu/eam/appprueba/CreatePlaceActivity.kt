3796d8356b41c79f18a0dd306dc803e45c9c948e
juan_camilo <juancamilovelez99@gmail.com>
2024-04-08 16:52:33 -0500

    ciclo de vida

---
+++ app/src/main/java/com/edu/eam/appprueba/CreatePlaceActivity.kt
@@ -0,0 +1,11 @@
+package com.edu.eam.appprueba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreatePlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_place)
+    }
+}
